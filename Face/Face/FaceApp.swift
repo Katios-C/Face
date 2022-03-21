321ff9f7b88380aa26f75f337e0caa7a6812ac95
Katios <kate.chonkina@gmail.com>
2022-03-21 10:42:29 +0600

    1

---
+++ Face/Face/FaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceApp.swift
+//  Face
+//
+//  Created by Екатерина Чернова on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
